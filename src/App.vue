5bf185402b12b76bf68ab0ea51c583a9ef30a7b1
Welinton Subtil <welinton@realizati.com.br>
2017-02-20 13:52:57 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>Welcome Vue 2</h1>
+    <h4> {{ user }} </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  computed: {
+    user () {
+      return `O usuário Logado é ${this.$store.state.user.name} e possui email ${this.$store.state.user.email}`
+    }
+  }
+}
+</script>
+</style>
---
+++ src/store/state.js
@@ -0,0 +1,8 @@
+export default {
+  user: {
+    name: 'Welinton Subtil ',
+    email: 'welinton@realizati.com.br',
+    level: '5'
+  },
+  token: 'dasadasdsad'
+}
